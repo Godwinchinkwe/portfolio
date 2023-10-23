f38df597cdcf82d17d731d69a27e93d7e89e71ad
Godwin Chinkwe <chinkwegodwin@gmail.com>
2023-10-23 10:37:19 +0100

    experience

--- src/Header/Experience/Experience.js
+++ src/Header/Experience/Experience.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { MdSchool } from 'react-icons/md';
+import "./Experience.css"
 
 const Experience = () => {
   return (
-    <div>
-      <h1> experience</h1>
+    <div className='experience'>
+      <h1> My Experience </h1> <br/>
+      <VerticalTimeline lineColor='red'>
+        <VerticalTimelineElement className='vertical-timeline-element--education' date="2010 -2014"
+        iconStyle={{background: "grey", color:"green"}}
+        icon={<MdSchool/>}>
+          <h3> My school high school, Abia state</h3>
+          <p>High School Diploma </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' date="2010 -2014"
+        iconStyle={{background: "grey", color:"green"}}
+        icon={<MdSchool/>}>
+          <h3> My school high school, Abia state</h3>
+          <p>High School Diploma </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' date="2010 -2014"
+        iconStyle={{background: "grey", color:"green"}}
+        icon={<MdSchool/>}>
+          <h3> My school high school, Abia state</h3>
+          <p>High School Diploma </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' date="2010 -2014"
+        iconStyle={{background: "grey", color:"green"}}
+        icon={<MdSchool/>}>
+          <h3> My school high school, Abia state</h3>
+          <p>High School Diploma </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' date="2010 -2014"
+        iconStyle={{background: "grey", color:"green"}}
+        icon={<MdSchool/>}>
+          <h3> My school high school, Abia state</h3>
+          <p>High School Diploma </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
   )
 }
